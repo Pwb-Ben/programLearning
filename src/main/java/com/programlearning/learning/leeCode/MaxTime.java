ec8f365b91b414dbf046b854d9769628f80dd1bf
panweibin <992733984@qq.com>
2020-05-15 21:52:14 +0800

    2020年5月15日21点52分

--- src/main/java/com/programlearning/learning/leeCode/MaxTime.java
+++ src/main/java/com/programlearning/learning/leeCode/MaxTime.java
@@ -1,9 +1,6 @@
 package com.programlearning.learning.leeCode;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 /**
  * 题目描述：给定一个数组，里面有6个整数，求这个数组能够表示的最大24进制的时间是多少，
@@ -18,6 +15,9 @@ import java.util.stream.Collectors;
  */
 public class MaxTime {
 
+    private static int[] maxList = new int[6];
+    private static int maxTime = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while(scanner.hasNextLine()){
@@ -27,29 +27,58 @@ public class MaxTime {
             for (int i = 0; i<sl.length; i++){
                 il[i] = Integer.parseInt(sl[i]);
             }
-            List list = Arrays.stream(il).boxed().collect(Collectors.toList());
+            solution(il, 0);
+            if (maxTime == 0){
+                System.out.println("invalid");
+            }else {
+                int hour, min, second;
+                hour = maxList[0]*10 + maxList[1];
+                min = maxList[2]*10 + maxList[3];
+                second = maxList[4]*10 + maxList[5];
+                System.out.printf("%02d:%02d:%02d",hour,min,second);
+                System.out.println();
+            }
+            maxTime = 0;
+        }
+    }
 
-            boolean invalidFlag = false;
-            int maxValue = 0, temp = 0, maxHours = 0, maxMin = 0, maxSecond = 0;
-            //先找小时数的最大值
-            for (int i = 0; i<list.size() && !invalidFlag; i++){
-                for (int j = 0; j<list.size() && !invalidFlag; j++){
-                    if (i != j){
-                        temp = il[i] * 10 + il[j];
-                        if (temp > maxValue && temp < 24 && temp > 0){
-                            maxValue = temp;
+    /**
+     * 递归调用函数交换数组中的数,得到全排列
+     */
+    private static void solution(int[] arr, int k) {
+        int n = arr.length;
+        if (k == n) {
+            int hour, min, second;
+            hour = arr[0]*10 + arr[1];
+            min = arr[2]*10 + arr[3];
+            second = arr[4]*10 + arr[5];
 
-                        }else {
-                            invalidFlag = true;
-                            System.out.println("invalid");
-                        }
-                    }
+            if (hour >= 24 || min >= 60 || second >= 60){
+
+            }else {
+                int totalSecond = hour * 3600 + min * 60  + second;
+                if (totalSecond > maxTime){
+                    maxTime = totalSecond;
+                    System.arraycopy(arr, 0, maxList, 0, 6);
                 }
             }
-            maxHours = maxValue;
+        }
 
+        for (int i = k; i < n; i++) {
+            swap(arr, k, i);
+            solution(arr, k + 1);
+            swap(arr, k, i);
+        }
+    }
 
-
+    /**
+     * 交换数组中两个数
+     */
+    private static void swap(int[] arr, int x, int y) {
+        if (x != y) {
+            int temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
         }
     }
 }
